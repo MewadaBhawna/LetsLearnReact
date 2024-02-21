5fcc7e667ab14f9efb948045f7a747212c5ee85c
MewadaBhawna <bhawnamewada08@gmail.com>
2024-02-21 09:17:53 +0100

    updates functional components

--- App.js
+++ App.js
@@ -1,6 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CompanyLogo from "./src/images/appLogo.jpeg";
+import BodyBg from "./src/images/flat-lay.jpg";
+import SearchIcon from "./src/images/search.svg";
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img className="appLogo" src={CompanyLogo} alt="appLogo" />
+      </div>
+      <div>
+        <ul className="navItems eczar-SemiBold">
+          <li className="listItems">Home</li>
+          <li className="listItems">About Us </li>
+          <li className="listItems">Contact Us </li>
+          <li className="listItems">Cart </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const BodyComponent = () => {
+  return (
+    <div className="bodyContainer">
+      <img className="bgImage" src={BodyBg} />
+      <div className="centered">
+        <input
+          className="searchBar"
+          type="search"
+          placeholder="Search for restaurants and food"
+        />
 
-const heading = React.createElement("h1", { id: "headId" }, "Hello from React");
+        <div className="eczar-normal">The best food of your choice</div>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="container">
+      <Header />
+      <BodyComponent />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 #React learning from Namste react course series updates
+
+#Parcel
+-Dev build
+-Local host(hosted our app locally)
+-HMR- Hot module replacement(Refreshing our page on change)
+-File watching algorithm- written in c++(on any change it build automatically on save)
+-Caching- Faster Builds
+-Differential bundling
+-Code spliting
+-Tree shaking
