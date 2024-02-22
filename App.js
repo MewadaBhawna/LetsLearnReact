99c94c379506a26723a9ac399c969afd6c82254a
MewadaBhawna <bhawnamewada08@gmail.com>
2024-02-22 16:05:21 +0100

    updates-4

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CompanyLogo from "./src/images/appLogo.jpeg";
 import BodyBg from "./src/images/flat-lay.jpg";
-import SearchIcon from "./src/images/search.svg";
+import Star from "./src/images/star.svg";
+import Time from "./src/images/time.svg";
+import Delivery from "./src/images/delivery.svg";
 const Header = () => {
   return (
     <div className="header">
@@ -20,6 +22,37 @@ const Header = () => {
     </div>
   );
 };
+const RestuarantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="vendor-image-tile vendor-list-li"
+        alt="restuarant image"
+        src={CompanyLogo}
+      />
+      <div className="vendor-tile-info">
+        <div className="res-name-rate">
+          <div>restuarant name</div>
+          <div>
+            <img className="rating" alt="rating" src={Star} />
+            <span style={{ color: "#ff7e73" }}>1</span>
+          </div>
+        </div>
+        <div>CUISINE</div>
+        <div className="time-tile">
+          <div>
+            <img style={{ "margin-right": "5px" }} alt="time" src={Time} />
+            60min
+          </div>
+          <div>
+            <img style={{ margin: " 0px 5px" }} alt="delivery" src={Delivery} />
+            Free
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 const BodyComponent = () => {
   return (
     <div className="bodyContainer">
@@ -32,6 +65,39 @@ const BodyComponent = () => {
         />
 
         <div className="eczar-normal">The best food of your choice</div>
+      </div>
+
+      <div style={{ padding: " 15px" }} className="eczar-SemiBold">
+        <span style={{ "font-size": "xx-large" }}>All restaurants</span>
+        <ul className="vendor-list-revamp">
+          <li className="vendor-list-li">
+            <RestuarantCard />
+          </li>
+          <li>
+            <RestuarantCard />
+          </li>
+          <li>
+            <RestuarantCard />
+          </li>
+          <li>
+            <RestuarantCard />
+          </li>
+          <li>
+            <RestuarantCard />
+          </li>
+          <li>
+            <RestuarantCard />
+          </li>
+          <li>
+            <RestuarantCard />
+          </li>
+          <li>
+            <RestuarantCard />
+          </li>
+          <li>
+            <RestuarantCard />
+          </li>
+        </ul>
       </div>
     </div>
   );
